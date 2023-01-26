9c0a2e7805fd706ce66ca9596c531b9f94c598aa
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-01-25 16:08:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,14 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage", page_icon='tada', layout='wide')
 
 st.header("Are you the xXDRAGONSLAYERXx?")
-st.subheader("Enter your name to find out if you are the DRAGONSLAYER")
+st.subheader("Enter your name to find out if you are the one true Dragon Slayer")
 name = st.text_input("What is your name?")
 
 def dragon(name):
     if name == "Jake":
-        st.header("Congrats, You are the DRAGONSLAYER")
+        st.header("Congrats, You are the 🐲 DRAGONSLAYER 🐲")
     elif name == "jake":
-        st.header("Congrats, You are the DRAGONSLAYER")
+        st.header("Congrats, You are the 🐲 DRAGONSLAYER 🐲")
     else:
         st.header("You are not the dragonslayer.")
 
